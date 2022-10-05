2b2573b8875338329759957280c96128c3e1326e
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2022-10-05 13:48:00 +0200

    var

--- 02-comment.php
+++ 02-comment.php
@@ -19,10 +19,11 @@ ceci est un commentaire html, il sera envoyé au navigateur
 
 <?php 
 
-// bonne pratique, ne ps fermer la balise php quand ce n'est pas utile 
+// bonne pratique, ne pas fermer la balise php quand ce n'est pas utile 
 
 // affichage avec echo, le point virgule est la fin de l'instruction, on peut mettre sans limite sur la même ligne, ou séparer les instructions par autant d'espace que voulu ( mauvaise pratique, mais PHP s'en fout). La fonction time () vous donne le timestamp (temps UNIX en secondes depuis le 01/01/1970)
 echo "<p> Temps Unix de la page :"; echo time();
 
 
-            echo "</p>"
+            echo "</p>";
+?>
